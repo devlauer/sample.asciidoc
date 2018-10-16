bc955967215807cbc167bf57563b3b326b98d5c9
dev.lauer <dev.lauer@elnarion.de>
2018-10-16 20:24:49 +0200

    Initial checkin of this sample

---
+++ project1/src/main/java/de/elnarion/sample/asciidoc/TestAsciidocClass.java
@@ -0,0 +1,14 @@
+package de.elnarion.sample.asciidoc;
+
+/**
+ * The Class TestAsciidocClass.
+ */
+public class TestAsciidocClass {
+
+	/**
+	 * Instantiates a new test asciidoc class.
+	 */
+	public TestAsciidocClass() {
+	}
+
+}
