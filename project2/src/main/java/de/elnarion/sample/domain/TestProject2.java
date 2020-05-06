469c8affde66d3fbd2bf63bcb3118af8c4c762c0
Lauer <dev.lauer@googlemail.com>
2020-05-06 21:40:41 +0200

    Extend sample project for sample class diagram over multiple modules

---
+++ project2/src/main/java/de/elnarion/sample/domain/TestProject2.java
@@ -0,0 +1,14 @@
+package de.elnarion.sample.domain;
+
+public class TestProject2 {
+    public BaseDomainClass baseDomainClassField;
+    private ChildA childA;
+
+    public ChildA getChildA() {
+        return childA;
+    }
+
+    public void setChildA(ChildA childA) {
+        this.childA = childA;
+    }
+}
